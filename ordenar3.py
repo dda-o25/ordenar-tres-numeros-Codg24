6af0ee8857b0675cfb71583de12b7137ad90d3df
Codg24 <carlos.diaz@iteso.mx>
2025-09-19 00:48:33 -0600

    Primera solución

--- ordenar3.py
+++ ordenar3.py
@@ -1,15 +1,39 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Integrantes
+Carlos Osvaldo Díaz García
+
+Fecha
+18 de septiembre de 2025
+
+Dados tres números, ordenarlos de mayor a menor.
 """
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+numero_1 = float(input("Primer número: "))
+numero_2 = float(input("Segundo número: "))
+numero_3 = float(input("Tercer número: "))
 
 # Proceso
+if numero_1 >= numero_2 and numero_1 >= numero_3:
+    if numero_2 >= numero_3:
+        # Salida
+        print("Números ordenados", numero_1, numero_2, numero_3)
+    else:
+        # Salida
+        print("Números ordenados", numero_1, numero_3, numero_2)
 
+elif numero_2 >= numero_1 and numero_2 >= numero_3:
+    if numero_1 >= numero_3:
+        # Salida
+        print("Números ordenados", numero_2, numero_1, numero_3)
+    else:
+        # Salida
+        print("Números ordenados", numero_2, numero_3, numero_1)
 
-# Salidas
-print(salida)
+else:
+    if a >= b:
+        # Salida
+        print("Números ordenados", numero_3, numero_1, numero_2)
+    else:
+        # Salida
+        print("Números ordenados", numero_3, numero_2, numero_1)
